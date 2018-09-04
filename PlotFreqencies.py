c0e6196007e27b3213285761717bafb8ef693ed2
Henry Qin <root@hq6.me>
2018-09-04 15:35:05 -0700

    Clean up PlotFrequencies

--- PlotFreqencies.py
+++ PlotFreqencies.py
@@ -3,21 +3,29 @@ from scipy.fftpack import fft, rfft
 from scipy.io import wavfile
 from sys import argv
 
-# FFT sample count
-N = 8192
-
 # Sampling frequency
 samples_per_second = 44100
 
-# Frequency resolution
-freq_resolution = samples_per_second / N
+# Number of samples
+N = 16384
 
-fs, data = wavfile.read(argv[1]) # load the data
-a = data.T # Assume single-channel track for now.
-b=[(ele/2**8.)*2-1 for ele in a] # this is 8-bit track, b is now normalized on [-1,1)
-c = fft(b, N) # calculate fourier transform (complex numbers list)
-d = len(c)/2 - 1  # you only need half of the fft list (real signal symmetry)
-frequencies = [x*freq_resolution for x in xrange(d)]
-print '\n'.join(",".join([str(f),str(x)]) for f, x in zip(frequencies, abs(c[:d])))
-plt.plot(frequencies, abs(c[:d]),'r')
+# Frequency resolution
+freq_resolution = samples_per_second * 1.0 / N
+
+# Read data
+fs, data = wavfile.read(argv[1])
+samples = data.T[N:2* N]
+
+# subtract the mean of the data
+mean_sample = sum(samples) * 1.0 / len(samples)
+samples = [x - mean_sample for x in samples]
+scaled_samples=[(ele/2**16.) for ele in samples]
+
+freq_vectors = fft(scaled_samples, N)
+num_buckets = len(freq_vectors)/2 - 1
+
+frequencies = [x*freq_resolution for x in xrange(num_buckets)]
+
+print '\n'.join(",".join([str(f),str(x)]) for f, x in zip(frequencies, abs(freq_vectors[:num_buckets])))
+plt.plot(frequencies, abs(freq_vectors[:num_buckets]),'r')
 plt.show()
