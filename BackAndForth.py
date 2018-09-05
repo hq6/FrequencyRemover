9698f07d6dd54b349a5809a6f4de725ee803bcbd
Henry Qin <root@hq6.me>
2018-09-05 01:06:17 -0700

    Switch to real FFT and demonstrate zeroing a frequency

--- BackAndForth.py
+++ BackAndForth.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from scipy.fftpack import fft, ifft
+from scipy.fftpack import rfft, irfft
 from scipy.io import wavfile
 from sys import argv
 import numpy as np
@@ -23,25 +23,28 @@ samples = data.T[S:S + N]
 # Subtract the mean of the data
 mean_sample = sum(samples) * 1.0 / len(samples)
 samples = [x - mean_sample for x in samples]
+
+# Scale data
 scaled_samples=[(ele/2**16.) for ele in samples]
 
-freq_vectors = fft(scaled_samples, N)
+freq_vectors = rfft(scaled_samples, N)
 num_buckets = len(freq_vectors)/2 - 1
 
 frequencies = [x*freq_resolution for x in xrange(num_buckets)]
 
 # Manipulate the fft
-# for i, freq in enumerate(frequencies):
-#     if abs(freq - 524.871826172) < 1:
-#         freq_vectors[i] = 0
+for i, freq in enumerate(frequencies):
+    if abs(freq - 524.871826172) < 1:
+        # This does't work if i == 0, but we ignore that case
+        freq_vectors[2*i-1] = 0
+        freq_vectors[2*i] = 0
 
 # Reverse the FFT
-rfft = ifft(freq_vectors)
-
+rfft = irfft(freq_vectors)
 
 # Verify correctness.
 print len(rfft)
 print len(samples)
 for x,y in zip(rfft, scaled_samples):
-    if abs(x - y) > 1e-10:
+    if abs(x - y) > 1e-3:
         print x,y
