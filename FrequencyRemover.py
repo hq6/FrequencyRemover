e2dd4b1a3f7815db119552671e3e777affb4ad37
Henry Qin <root@hq6.me>
2018-09-07 17:19:08 -0700

    Convert frequency radius to a parameter

--- FrequencyRemover.py
+++ FrequencyRemover.py
@@ -8,7 +8,7 @@ import numpy as np
 
 # Remove frequencies with a starting sample of S, a sampling rate of R, N
 # samples per block, and B blocks.
-def removeFrequencies(infile, outfile, removedFrequencies = list(), S = 0, B = 1, R = 44100, N = 16384):
+def removeFrequencies(infile, outfile, removedFrequencies = list(), S = 0, B = 1, R = 44100, N = 16384, radius = 8):
     # Frequency resolution
     freq_resolution = R * 1.0 / N
 
@@ -29,7 +29,7 @@ def removeFrequencies(infile, outfile, removedFrequencies = list(), S = 0, B = 1
 	# Manipulate the fft
 	for i, freq in enumerate(frequencies):
           for rf in removedFrequencies:
-	    if abs(freq - rf) < 5:
+	    if abs(freq - rf) < radius:
 		# This does't work if i == 0, but we ignore that case
 		freq_vectors[2*i-1] = 0
 		freq_vectors[2*i] = 0
@@ -43,4 +43,4 @@ def removeFrequencies(infile, outfile, removedFrequencies = list(), S = 0, B = 1
     # Write output
     wavfile.write(outfile, R, data.T)
 
-removeFrequencies(argv[1], argv[2], [525], 32768, 10)
+removeFrequencies(argv[1], argv[2], [525], 32768, N=163840)
